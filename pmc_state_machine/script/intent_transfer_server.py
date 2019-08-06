b743a31c54a45b0b6538c29aeca3fe6d52e15135
shannon112 <b03505011@ntu.edu.tw>
2019-08-06 21:36:00 +0800

    changed navigation name and add reset trigger

--- pmc_state_machine/script/intent_transfer_server.py
+++ pmc_state_machine/script/intent_transfer_server.py
@@ -3,10 +3,12 @@
 import rospy
 from std_msgs.msg import String
 from std_srvs.srv import Trigger, TriggerResponse
+from smach_msgs.msg import SmachContainerStatus
 
 intent=""
 intentId=0
 sentId=0
+waitingFlag = 1
 
 def sendingVCommand(req):
     global intent
@@ -31,9 +33,27 @@ def storeIntent(data):
     intentId += 1
     print "server echo /Intent = ",intent
 
+def resetStatus(data):
+    global waitingFlag
+    if data.path == "/SM_ROOT":
+        if data.active_states[0] == "VoiceCommand" and waitingFlag == 0:
+            pub = rospy.Publisher('chatter', String, queue_size=10)
+            pub.publish(String("reset current state to blank"))
+            print("hi")
+            waitingFlag = 1
+        elif data.active_states[0] == "VoiceCommand":
+            waitingFlag = 1
+        elif  data.active_states[0] == "NAVI_D":
+            waitingFlag = 0
+        elif  data.active_states[0] == "NAVI_F":
+            waitingFlag = 0
+
 def main():
     rospy.init_node('intent_transfer_server')
+    # subscribing /Intent continuously and call service once when change
     rospy.Subscriber('/Intent', String, storeIntent)
+    # subscribing /server_pmc/smach/container_status and pub to /chatter once when change
+    rospy.Subscriber('/server_pmc/smach/container_status', SmachContainerStatus, resetStatus)
     s = rospy.Service('triggerVCommand', Trigger, sendingVCommand)
     rospy.spin()
 
